7833b0d88d0101612bb38261dbde2d13e8d94fe8
Two J <TwoJ@gmail.com>
2023-07-05 11:10:24 +0630

    add-cors

--- server/index.js
+++ server/index.js
@@ -31,6 +31,15 @@ app.use(
     origin: ["https://mern-social-beta.vercel.app/"],
   })
 );
+app.use((req, res, next) => {
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://mern-social-beta.vercel.app/"
+  );
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
 /*Files Storage */
