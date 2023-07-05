3bc157fd962781933e2a8e71d631d58cc95e12e9
Two J <TwoJ@gmail.com>
2023-07-05 13:40:10 +0630

    ll

--- server/index.js
+++ server/index.js
@@ -31,13 +31,16 @@ app.use(express.urlencoded({ limit: "30mb", extended: true }));
 //     origin: ["https://mern-social-beta.vercel.app/"],
 //   })
 // );
-// app.use((req, res, next) => {
-//   res.setHeader("Access-Control-Allow-Credentials", true);
-//   res.setHeader("Access-Control-Allow-Origin", "*");
-//   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-//   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-//   next();
-// });
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,OPTIONS, POST, PUT, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
 
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
