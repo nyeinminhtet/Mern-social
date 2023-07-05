71f7cb2a168c21329c08469ca6fdcbaa09d75d29
Two J <TwoJ@gmail.com>
2023-07-05 10:54:23 +0630

    add-vercel

--- server/index.js
+++ server/index.js
@@ -26,7 +26,11 @@ app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors());
+app.use(
+  cors({
+    origin: ["https://mern-social-beta.vercel.app/"],
+  })
+);
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
 /*Files Storage */
