c3fa0e3f81aafad0bfb66236cb7d5b6f37804b5f
Two J <TwoJ@gmail.com>
2023-07-05 13:37:13 +0630

    remove-already-fixed

--- server/index.js
+++ server/index.js
@@ -26,21 +26,18 @@ app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
-app.use(
-  cors({
-    origin: ["https://mern-social-beta.vercel.app/"],
-  })
-);
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    "https://mern-social-beta.vercel.app"
-  );
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  next();
-});
+// app.use(
+//   cors({
+//     origin: ["https://mern-social-beta.vercel.app/"],
+//   })
+// );
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Credentials", true);
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+//   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+//   next();
+// });
 
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
